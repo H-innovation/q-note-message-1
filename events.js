55b4bf25a5e8a469004199f8956fd42593986ce1
Curtis <curtis.campbell@hermes-europe.co.uk>
2021-08-19 15:11:50 +0100

    updated click listner

--- events.js
+++ events.js
@@ -6,6 +6,7 @@ AFRAME.registerComponent("markerhandler", {
 		// every click, we make our model grow in size :)
 		animatedMarker.addEventListener("click", function (ev, target) {
 			const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+			alert("item clicked");
 			if (aEntity && intersectedElement === aEntity) {
 				const scale = aEntity.getAttribute("scale");
 				Object.keys(scale).forEach((key) => (scale[key] = scale[key] + 1));
